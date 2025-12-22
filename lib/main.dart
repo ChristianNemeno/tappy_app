4197163e777c0e787360939e50d26fc458b61d4e
christian <christiannemeno80@gmail.com>
2025-12-22 15:46:29 +0800

    home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tappy_app/providers/auth_provider.dart';
 import 'package:tappy_app/screens/login_screen.dart';
+import 'package:tappy_app/services/auth_service.dart';
+import 'package:tappy_app/utils/api_client.dart';
+
+
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final authProvider = Provider.of<AuthProvider>(context, listen: false);
+
+    return Scaffold(      
+      appBar: AppBar(
+        title: Text('Welcome ${authProvider.authData?.userName ?? 'User'}'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('You are logged in!'),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () => authProvider.logout(),
+            child: const Text('Logout'),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 void main() {
   runApp(const MyApp());
@@ -14,21 +47,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+       
       colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 4, 80, 2)),
       ),
       home: const LoginScreen(),
