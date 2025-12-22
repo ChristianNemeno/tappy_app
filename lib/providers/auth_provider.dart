8d5089486b4af183af79b7bc944e305052893f94
christian <christiannemeno80@gmail.com>
2025-12-22 09:11:14 +0800

    finsished auth provider

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -12,17 +12,79 @@ class AuthProvider extends ChangeNotifier {
   bool _isLoading = true;
   String? _error;
 
+  // these are just getters
   AuthResponse? get authData => _authData;
   bool get isLoading => _isLoading;
   String? get error => _error;
   bool get isAuthenticated => _authData != null;
   
   AuthProvider(this._authService){
-    
+    _tryAutoLogin();
   }
 
   Future<void> _tryAutoLogin() async {
+      try{
+        _authData = await _authService.getSavedAuthData();
+      }catch(e){
+        _authData = null;
+        _error = e.toString();
+      }
+      _isLoading = false;
+      notifyListeners();
+  }
+
+  Future<bool> login(String email, String password) async {
+    _setLoading(true);
+    try{
+      final result = await _authService.login(email, password);
+      _authData = result;
+      _setLoading(false);
+      return true;
+    }catch(e){
+      _setError(e.toString());
+      _setLoading(false);
+      return false;
+    }
+
+  }
+
+  Future<bool> register(String username, String email, String password) async
+  {
+    _setLoading(true);
+
+    try{
+      final result = await _authService.register(username, email, password);
+      _authData = result;
+      _setLoading(false);
+      return true;
+    }catch(e){
+      _setError(e.toString());
+      return false;
+    }
+  }
+
+
+  Future<void> logout() async {
     
+    _authData = null;
+    await _authService.clearToken();
+    notifyListeners();
+  }
+
+  
+
+
+
+
+  void _setLoading(bool value) {
+    _isLoading = value;
+    _error = null;  
+    notifyListeners();
+  }
+  void _setError(String error) {
+    _error = error;
+    _isLoading = false;
+    notifyListeners();
   }
 
 
