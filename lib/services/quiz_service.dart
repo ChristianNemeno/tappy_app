001f5ec87c038b983023d6ed3d0e5196e461c2e9
christian <christiannemeno80@gmail.com>
2025-12-24 21:08:00 +0800

    add QuizService to handle quiz-related API interactions

---
+++ lib/services/quiz_service.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import '../models/quiz.dart';
+import '../utils/api_client.dart';
+
+class QuizService {
+  final ApiClient _apiClient;
+
+  QuizService(this._apiClient);
+
+  Future<List<Quiz>> getActiveQuizzes() async {
+    final response = await _apiClient.get('/quiz/active');
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      return data.map((json) => Quiz.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load quizzes');
+    }
+  }
+
+  Future<Quiz> getQuizById(int id) async {
+    final response = await _apiClient.get('/quiz/$id');
+
+    if (response.statusCode == 200) {
+      return Quiz.fromJson(json.decode(response.body));
+    } else if (response.statusCode == 404) {
+      throw Exception('Quiz not found');
+    } else {
+      throw Exception('Failed to load quiz details');
+    }
+  }
+}
