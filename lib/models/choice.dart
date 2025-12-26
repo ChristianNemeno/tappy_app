45e45bd74d50fffc2832366cfba1cc7d962ae4df
christian <christiannemeno80@gmail.com>
2025-12-26 21:37:59 +0800

    feat: Add Choice model for quiz options

---
+++ lib/models/choice.dart
@@ -0,0 +1,31 @@
+class Choice {
+  final int id;
+  final String text;
+  final bool? isCorrect; // Nullable - not sent during quiz taking, only in results
+  final int questionId;
+
+  Choice({
+    required this.id,
+    required this.text,
+    this.isCorrect,
+    required this.questionId,
+  });
+
+  factory Choice.fromJson(Map<String, dynamic> json) {
+    return Choice(
+      id: json['Id'],
+      text: json['Text'] ?? '',
+      isCorrect: json['IsCorrect'],
+      questionId: json['QuestionId'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'Id': id,
+      'Text': text,
+      if (isCorrect != null) 'IsCorrect': isCorrect,
+      'QuestionId': questionId,
+    };
+  }
+}
